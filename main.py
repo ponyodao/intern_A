63208fe862875883bc26c53d917cff4e01b28581
Ryotaro <cguf1028@mail4.doshisha.ac.jp>
2022-08-25 18:04:20 +0900

    final commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from audioop import add
+import imp
 from saitama_road import saitama_road
 from saitama_water import saitama_water
 from chiba_road import chiba_road
@@ -6,27 +7,46 @@ from send import send_mail
 from get_list import get_item
 from chiba_water import chiba_water
 from divide_address import divide_address
+import threading
+import time
 
+while True:
 
+    mail = get_item( "is:unread", 1)
 
-mail = get_item( "is:unread", 1)
-divide = mail[0].split("<")
-sender = divide[1].replace(">","")
-address = mail[1]
-
-if mail:
-    if("千葉県" in address and "千葉市" in address):
-        chiba_road(address)
-        chiba_water(address)
-        
-        send_mail("intern.ohg.24a@gmail.com", sender, "地図情報を送信", "", "C:\\Users\\ryo2001\\OneDrive - 同志社大学\\デスクトップ\\エンカレッジ\\オープンハウス\\test\\RPA\\intern_A\\img\\chiba_water.pdf", cc=None)
-        send_mail("intern.ohg.24a@gmail.com", sender, "地図情報を送信", "", "C:\\Users\\ryo2001\\OneDrive - 同志社大学\\デスクトップ\\エンカレッジ\\オープンハウス\\test\\RPA\\intern_A\\img\\chiba_road.pdf", cc=None)
-    elif("埼玉県" in address and "さいたま市" in address):
-        saitama_road(address)
-        saitama_water(address)
-        send_mail("intern.ohg.24a@gmail.com", sender, "地図情報を送信", "", "C:\\Users\\ryo2001\\OneDrive - 同志社大学\\デスクトップ\\エンカレッジ\\オープンハウス\\test\\RPA\\intern_A\\img\\saitama_water.pdf", cc=None)
-        send_mail("intern.ohg.24a@gmail.com", sender, "地図情報を送信", "", "C:\\Users\\ryo2001\\OneDrive - 同志社大学\\デスクトップ\\エンカレッジ\\オープンハウス\\test\\RPA\\intern_A\\img\\saitama_road.pdf", cc=None)
+    try:
+        divide = mail[0].split("<")
+    except TypeError:
+        print('新規メールなし')
     else:
-        send_mail("intern.ohg.24a@gmail.com", sender, "", "千葉県、あるいは埼玉県の住所を入力してください。", "", cc=None)
-    
+        sender = divide[1].replace(">","")
+        address = mail[1]
+
+        if mail:
+            if("千葉県" in address and "千葉市" in address):
+                
+                t1 = time.time()
+                chiba_water(address)
+                chiba_road(address)
+                # th1 = threading.Thread(target = chiba_water(address))
+                # th2 = threading.Thread(target = chiba_road(address))
+               
+                # th1.start()
+                # th2.start()
+                send_mail("intern.ohg.24a@gmail.com", sender, "千葉の下水道地図情報", "", "C:\\Users\\ryo2001\\OneDrive - 同志社大学\\デスクトップ\\エンカレッジ\\オープンハウス\\test\\RPA\\intern_A\\img\\chiba_water.pdf", cc=None)
+                send_mail("intern.ohg.24a@gmail.com", sender, "千葉の道路地図情報", "", "C:\\Users\\ryo2001\\OneDrive - 同志社大学\\デスクトップ\\エンカレッジ\\オープンハウス\\test\\RPA\\intern_A\\img\\chiba_road.pdf", cc=None)
+                t2 = time.time()
+
+                elapsed_time = t2-t1
+                print(f'経過時間：{elapsed_time}')
+            elif("埼玉県" in address and "さいたま市" in address):
+                
+                saitama_water(address)
+                send_mail("intern.ohg.24a@gmail.com", sender, "埼玉の下水道地図情報", "", "C:\\Users\\ryo2001\\OneDrive - 同志社大学\\デスクトップ\\エンカレッジ\\オープンハウス\\test\\RPA\\intern_A\\img\\saitama_water.pdf", cc=None)
+                
+                saitama_road(address)
+                send_mail("intern.ohg.24a@gmail.com", sender, "埼玉の道路地図情報", "", "C:\\Users\\ryo2001\\OneDrive - 同志社大学\\デスクトップ\\エンカレッジ\\オープンハウス\\test\\RPA\\intern_A\\img\\saitama_road.pdf", cc=None)
+            else:
+                send_mail("intern.ohg.24a@gmail.com", sender, "", "県名から正確な住所を入力してください。", "", cc=None)
+        
 
